a30a9dfd6eddc4393925eb54bdabfd69a2396be5
OscarJuJi <89614319+OscarJuJi@users.noreply.github.com>
2023-08-15 20:54:21 +0000

    title changed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,13 +22,13 @@ import PropTypes from "prop-types";
  */
 import image from "../images/background.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "A simple web code";
 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "25rem", color: "#FFFFF" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
